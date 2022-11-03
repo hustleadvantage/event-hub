2063264611f7475a773081ee8bcf7270c63e28a5
Hassan Robinson <24365381+hassanrobinson@users.noreply.github.com>
2022-11-03 17:20:29 -0400

    css tokens and theme updates

--- apps/web/src/hooks.server.ts
+++ apps/web/src/hooks.server.ts
@@ -1,23 +1,17 @@
+import { CONFIG } from '$client/constants';
 import { Theme } from '$client/features';
-import {
-	PUBLIC_COLOR_ACCENT,
-	PUBLIC_COLOR_PRIMARY,
-	PUBLIC_COLOR_SECONDARY
-} from '$env/static/public';
 import type { Handle } from '@sveltejs/kit';
 import { string } from 'fp-ts';
 import { pipe } from 'fp-ts/lib/function';
 
-const COLOR_PRIMARY = PUBLIC_COLOR_PRIMARY ?? 'teal';
-const COLOR_SECONDARY = PUBLIC_COLOR_SECONDARY ?? 'violet';
-const COLOR_ACCENT = PUBLIC_COLOR_ACCENT ?? 'blue';
+const { COLORS } = CONFIG.THEME;
 
 export const handle: Handle = async ({ event, resolve }) => {
-	const colors = { primary: COLOR_PRIMARY, secondary: COLOR_SECONDARY, accent: COLOR_ACCENT };
+	const colors = { primary: COLORS.PRIMARY, secondary: COLORS.SECONDARY, accent: COLORS.ACCENT };
+	const cssTokens = Theme.createCssTokens({ colors });
 
 	const response = await resolve(event, {
-		transformPageChunk: ({ html }) =>
-			pipe(html, string.replace('%cssVars%', Theme.createCssTokens({ colors })))
+		transformPageChunk: ({ html }) => pipe(html, string.replace('%cssTokens%', cssTokens))
 	});
 
 	return response;
--- apps/web/src/lib/client/constants/config.ts
+++ apps/web/src/lib/client/constants/config.ts
@@ -1,29 +1,54 @@
-import {
-	PUBLIC_FACEBOOK_PIXEL_ID,
-	PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID,
-	PUBLIC_GOOGLE_MAPS_KEY,
-	PUBLIC_IPINFO_PUBLIC_KEY,
-	PUBLIC_PLAUSIBLE_DOMAIN
-} from '$env/static/public';
-
 import { option, string } from 'fp-ts';
+import { pipe } from 'fp-ts/lib/function';
 
-const envVarOption = (value: string) =>
-	option.fromPredicate((s: string) => !string.isEmpty(s))(value);
+// Facebook
+const FACEBOOK_PIXEL_ID = import.meta.env.VITE_FACEBOOK_PIXEL_ID;
+
+// Google
+const GOOGLE_ANALYTICS_MEASUREMENT_ID = import.meta.env.VITE_GOOGLE_ANALYTICS_MEASUREMENT_ID;
+const GOOGLE_MAPS_KEY = import.meta.env.VITE_GOOGLE_MAPS_KEY;
+
+// IP Info
+const IPINFO_KEY = import.meta.env.VITE_IPINFO_PUBLIC_KEY;
+
+// Plausible
+const PLAUSIBLE_DOMAIN = import.meta.env.VITE_PLAUSIBLE_DOMAIN;
+
+// Theme
+const COLOR_PRIMARY = import.meta.env.VITE_COLOR_PRIMARY;
+const COLOR_SECONDARY = import.meta.env.VITE_COLOR_SECONDARY;
+const COLOR_ACCENT = import.meta.env.VITE_COLOR_ACCENT;
+
+const varOrNone = (value: string | undefined) =>
+	pipe(
+		option.fromNullable(value),
+		option.chain(option.fromPredicate((s: string) => !string.isEmpty(s)))
+	);
+
+const defaultOrValue = <T>(defaultValue: T, value: option.Option<T>) =>
+	option.getOrElse(() => defaultValue)(value);
 
 export const FACEBOOK = {
-	PIXEL_ID: envVarOption(PUBLIC_FACEBOOK_PIXEL_ID)
+	PIXEL_ID: varOrNone(FACEBOOK_PIXEL_ID)
 };
 
 export const GOOGLE = {
-	ANALYTICS_MEASUREMENT_ID: envVarOption(PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID),
-	MAPS_PUBLIC_KEY: envVarOption(PUBLIC_GOOGLE_MAPS_KEY)
+	ANALYTICS_MEASUREMENT_ID: varOrNone(GOOGLE_ANALYTICS_MEASUREMENT_ID),
+	MAPS_PUBLIC_KEY: varOrNone(GOOGLE_MAPS_KEY)
 };
 
 export const IPINFO = {
-	PUBLIC_KEY: envVarOption(PUBLIC_IPINFO_PUBLIC_KEY)
+	PUBLIC_KEY: varOrNone(IPINFO_KEY)
 };
 
 export const PLAUSIBLE = {
-	DOMAIN: envVarOption(PUBLIC_PLAUSIBLE_DOMAIN)
+	DOMAIN: varOrNone(PLAUSIBLE_DOMAIN)
+};
+
+export const THEME = {
+	COLORS: {
+		PRIMARY: defaultOrValue('violet', varOrNone(COLOR_PRIMARY)),
+		SECONDARY: defaultOrValue('teal', varOrNone(COLOR_SECONDARY)),
+		ACCENT: defaultOrValue('blue', varOrNone(COLOR_ACCENT))
+	}
 };
