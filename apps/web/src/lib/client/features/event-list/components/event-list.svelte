e912158df3b63da54f5a3ffefa57634981ea7c42
Hassan Robinson <24365381+hassanrobinson@users.noreply.github.com>
2022-11-04 01:33:23 -0400

    updates

--- apps/web/src/lib/client/features/event-list/components/event-list.svelte
+++ apps/web/src/lib/client/features/event-list/components/event-list.svelte
@@ -1,5 +1,6 @@
 <script lang='ts'>
 	import { Title } from '$client/components';
+	import Heading from '$client/components/heading/heading.svelte';
 	import { CONFIG } from '$client/constants';
 	import EventListFilters from './event-list-filters.svelte';
   import EventListItem from './event-list-item.svelte';
@@ -16,10 +17,12 @@
 
 <div class='max-w-screen-md md:max-w-screen-lg mx-auto px-5 py-8'>
   <div class='grid gap-10'>
-    <Title>
-      {businessName}
-      <span class='block mt-2 text-4xl font-normal'>Upcoming Events</span>
-    </Title>
+    <div class='grid gap-2'>
+      <Title>
+        {businessName}
+      </Title>
+      <Heading level={2}><span class='font-normal'>Upcoming Events</span></Heading>
+    </div>
     <EventListFilters />
     {#if $eventsStore.length}
       <div class='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8'>
